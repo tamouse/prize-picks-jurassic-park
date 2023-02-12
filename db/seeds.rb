0cd951158fb39b118a3449d340037518c61ac892
Tamara Temple <tamouse@gmail.com>
2023-02-11 18:12:43 -0600

    Add db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+vores = Vore::VORE_TYPES.map do |v|
+  Vore.find_or_create_by(name: v)
+end
+
+vores.map do |v|
+  species_list = v.name == 'herbivore' ? Species::HERBIVORE_SPECIES : Species::CARNIVORE_SPECIES
+  species_list.map do |s|
+    Species.find_or_create_by(name: s, vore: v)
+  end
+end
+
+pp Vore.all.pluck(:id, :name)
+pp Species.all.pluck(:id, :name, :vore_id)
